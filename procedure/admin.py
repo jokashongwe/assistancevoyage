540f1f111f51337158b8c93ece8f113bb7bcdb1d
Jonathan Kashongwe <jokashongwe@gmail.com>
2026-01-10 16:23:11 +0100

    Fix values

--- procedure/admin.py
+++ procedure/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Dossier, FichierClient, TypeDocument
+from .models import Dossier, FichierClient, TypeDocument, Offre
 
 admin.site.register(Dossier)
 admin.site.register(FichierClient)
 admin.site.register(TypeDocument)
+admin.site.register(Offre)
 
 
 # Register your models here.
